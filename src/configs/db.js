294cf849afc1e89a1e0ab975656a65a3264987c4
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-04-04 00:40:14 -0300

    restore

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,10 +1,10 @@
 module.exports = {
-  dialect: process.env.DIALECT,
-  host: process.env.HOST,
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-  port: process.env.DB_PORT,
+  dialect: 'mysql', // process.env.DIALECT,
+  host: 'localhost', // process.env.HOST,
+  username: 'root', // process.env.USERNAME,
+  password: 'root', // process.env.PASSWORD,
+  database: 'instadev', // process.env.DATABASE,
+  port: 3308, // process.env.DB_PORT,
   define: {
     timestamps: true,
     underscored: true,
