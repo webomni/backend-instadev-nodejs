f4b88babc7ea451aeb2deea1116017e7fa838204
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-04-11 16:43:35 -0300

    create schema validator to user and create middleware to validate schema

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,12 @@
 const { Router } = require('express');
+const schemaValidator = require('./apps/middlewares/schemaValidator');
+
+const UserControler = require('./apps/controllers/UserController');
+const userSchema = require('./schema/create.user.schema.json');
 
 const routes = new Router();
+
+routes.post('/user', schemaValidator(userSchema), UserControler.create);
 
 routes.get('/health', (req, res) => res.send({
   message: 'Connected with success!',
