b687c815a31b29b89aa9325408953f7270b0c0b1
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-04-11 19:51:52 -0300

    create route, controller and jsonschema to authentication

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,17 @@
 const { Router } = require('express');
 const schemaValidator = require('./apps/middlewares/schemaValidator');
 
+const AuthenticationController = require('./apps/controllers/AuthenticationController');
+const authSchema = require('./schema/auth.schema.json');
+
 const UserControler = require('./apps/controllers/UserController');
 const userSchema = require('./schema/create.user.schema.json');
 
 const routes = new Router();
 
 routes.post('/user', schemaValidator(userSchema), UserControler.create);
+
+routes.post('/auth', schemaValidator(authSchema), AuthenticationController.authenticate);
 
 routes.get('/health', (req, res) => res.send({
   message: 'Connected with success!',
