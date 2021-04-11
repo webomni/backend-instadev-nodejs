b278a8cee0c1eb1bfa318570e23cdcdccca2cd3b
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-04-11 19:52:10 -0300

    created feature to generate password_hash and method checkPassword

--- src/apps/models/Users.js
+++ src/apps/models/Users.js
@@ -1,5 +1,6 @@
 const Sequelize = require('sequelize');
 const { Model } = require('sequelize');
+const bcryptjs = require('bcryptjs');
 
 class Users extends Model {
   static init(sequelize) {
@@ -18,7 +19,18 @@ class Users extends Model {
         sequelize,
       },
     );
+
+    this.addHook('beforeSave', async (user) => {
+      if (user.password) {
+        user.password_hash = await bcryptjs.hash(user.password, 8);
+      }
+    });
+
     return this;
+  }
+
+  checkPassword(password) {
+    return bcryptjs.compare(password, this.password_hash);
   }
 }
 
