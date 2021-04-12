5340f9a9c17fd27f6466cd1ee7192248db8927c1
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-04-11 23:02:17 -0300

    create route to add like

--- src/routes.js
+++ src/routes.js
@@ -35,5 +35,6 @@ routes.post('/upload', upload.single('image'), FileController.upload);
 routes.post('/post', schemaValidator(postSchema), PostController.create);
 routes.delete('/post/:id', PostController.delete);
 routes.put('/post/:id', PostController.update);
+routes.put('/add-like/:id', PostController.addLike);
 
 module.exports = routes;
