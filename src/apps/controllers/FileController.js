1fc3bc0c50e7b27f7e6d565e01c1a3cebbf41cfe
danieldeandradelopes <danieldeandradelopes@gmail.com>
2021-08-27 08:49:55 -0300

    created integration to backblaze

--- src/apps/controllers/FileController.js
+++ src/apps/controllers/FileController.js
@@ -1,8 +1,56 @@
+require('dotenv').config();
+
+const fsp = require('fs/promises');
+const B2 = require('backblaze-b2');
+
+const {
+  APPLICATION_KEY_ID,
+  APPLICATION_KEY,
+  BUCKET_ID,
+  BASE_URL_BACKBLAZE
+} = process.env;
+
+const b2 = new B2({
+  applicationKeyId: APPLICATION_KEY_ID,
+  applicationKey: APPLICATION_KEY
+});
+
+const unlinkAsync = fsp.unlink;
 class FileController {
   async upload(req, res) {
-    const { filename } = req.file;
+    const { filename, path } = req.file;
 
-    return res.send({ url: `uploads/${filename}` });
+    try {
+
+      const file = await fsp.readFile(`uploads/${filename}`, (err, data) => {
+        if (err) {
+          throw err
+        }
+
+        return data;
+      });
+
+      await b2.authorize();
+
+      const { data: { uploadUrl, authorizationToken } } = await b2.getUploadUrl({
+        bucketId: BUCKET_ID
+      });
+
+      const { data } = await b2.uploadFile({
+        uploadUrl: uploadUrl,
+        uploadAuthToken: authorizationToken,
+        fileName: filename,
+        data: file
+      });
+
+      await unlinkAsync(path);
+
+      return res.send({ url: `${BASE_URL_BACKBLAZE}${data.fileName}` });
+
+    } catch (error) {
+      return res.status(400).send({ message: 'Failed to upload!' });
+    }
+
   }
 }
 
